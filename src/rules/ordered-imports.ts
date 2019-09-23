107a4379e5dfab228137aac0435d795db3dc018c
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-09-23 20:24:01 +0900

    Improved ordered-imports rule

--- src/rules/ordered-imports.ts
+++ src/rules/ordered-imports.ts
@@ -1,36 +1,47 @@
 import _ from 'lodash';
-import { ImportDeclaration, Statement, Token } from '@typescript-eslint/typescript-estree/dist/ts-estree/ts-estree';
+import { ImportDeclaration, Statement } from '@typescript-eslint/typescript-estree/dist/ts-estree/ts-estree';
 import { ESLintUtils } from '@typescript-eslint/experimental-utils';
+import { SourceCode, RuleFixer, RuleFix } from '@typescript-eslint/experimental-utils/dist/ts-eslint';
 
-const isImport = (statement: Statement): statement is ImportDeclaration => {
-  return statement.type === 'ImportDeclaration';
-};
+const isImport = (statement: Statement): statement is ImportDeclaration => statement.type === 'ImportDeclaration';
 
-const getReportNode = (a: ImportDeclaration[], b: ImportDeclaration[]): ImportDeclaration => {
-  const [, [, reportNode]] = _.zip(a, b)
-    .map(([a, b]): [boolean, ImportDeclaration[]] => [a.source.value === b.source.value, [a, b]])
-    .filter(([bool]) => !bool)
-    .shift();
-
-  return reportNode;
-};
-
-const sortImportStatements = (importStatements: ImportDeclaration[]): ImportDeclaration[] => {
-  return _.sortBy(
+const sortImportStatements = (importStatements: ImportDeclaration[]): ImportDeclaration[] =>
+  _.sortBy(
     importStatements,
     ({ source: { value } }) => {
-      const v = _.toString(value);
-      const vv = v.replace('/', '');
-      const vvv = vv.match(/^\.+/);
+      const [leadingDots] =
+        _.toString(value)
+          .replace('/', '')
+          .match(/^\.+/) || [];
 
-      if (vvv) {
-        return vvv[0].length * -1;
-      }
-
-      return -Infinity;
+      return leadingDots ? leadingDots.length * -1 : -Infinity;
     },
     ({ source: { value } }) => value,
   );
+
+const details = (
+  sourceCode: SourceCode,
+  node: ImportDeclaration,
+): {
+  nodeText: string;
+  everythingBeforeExceptFirstLine: string;
+  everythingAfterSameLine: string;
+} => {
+  const tokenBefore = sourceCode.getTokenBefore(node);
+  const tokenAfter = sourceCode.getTokenAfter(node);
+
+  const nodeText = sourceCode.getText(node);
+  const everythingBefore = sourceCode
+    .getText(node, node.range[0] - (tokenBefore ? tokenBefore.range[1] : 0), 0)
+    .replace(new RegExp(`${_.escapeRegExp(nodeText)}$`), '');
+  const everythingAfter = sourceCode
+    .getText(node, 0, (tokenAfter ? tokenAfter.range[0] : 0) - node.range[1])
+    .replace(new RegExp(`^${_.escapeRegExp(nodeText)}`), '');
+
+  const everythingAfterSameLine = everythingAfter.substring(0, everythingAfter.indexOf('\n'));
+  const everythingBeforeExceptFirstLine = everythingBefore.substring(everythingBefore.indexOf('\n'));
+
+  return { nodeText, everythingBeforeExceptFirstLine, everythingAfterSameLine };
 };
 
 export default ESLintUtils.RuleCreator(name => name)({
@@ -63,81 +74,39 @@ export default ESLintUtils.RuleCreator(name => name)({
           return;
         }
 
-        const unsortedImportStatements = initialImportStatements.reduce<
-          { actual: ImportDeclaration; expected: ImportDeclaration }[]
-        >((previousValue, currentValue, i) => {
-          if (currentValue !== sortedImportStatements[i]) {
-            return [
-              ...previousValue,
-              {
-                actual: currentValue,
-                expected: sortedImportStatements[i],
-              },
-            ];
-          }
-
-          return previousValue;
-        }, []);
+        const unsortedImportStatements = _.zip(initialImportStatements, sortedImportStatements).map(
+          ([actual, expected]) => ({
+            actual: actual as ImportDeclaration,
+            expected: expected as ImportDeclaration,
+          }),
+        );
 
         const sourceCode = context.getSourceCode();
 
-        const details = (
-          node: ImportDeclaration,
-        ): {
-          node: ImportDeclaration;
-          tokenBefore: Token;
-          tokenAfter: Token;
-          nodeText: string;
-          everythingBefore: string;
-          everythingAfter: string;
-          everythingAfterSameLine: string;
-        } => {
-          const tokenBefore = sourceCode.getTokenBefore(node);
-          const tokenAfter = sourceCode.getTokenAfter(node);
-
-          const nodeText = sourceCode.getText(node);
-          const everythingBefore = sourceCode
-            .getText(node, node.range[0] - (tokenBefore ? tokenBefore.range[1] : 0), 0)
-            .replace(new RegExp(`${_.escapeRegExp(nodeText)}$`), '');
-          const everythingAfter = sourceCode
-            .getText(node, 0, (tokenAfter ? tokenAfter.range[0] : 0) - node.range[1])
-            .replace(new RegExp(`^${_.escapeRegExp(nodeText)}`), '');
-
-          const everythingAfterSameLine = everythingAfter.substring(0, everythingAfter.indexOf('\n'));
-
-          return {
-            node,
-            tokenBefore,
-            tokenAfter,
-            nodeText,
-            everythingBefore,
-            everythingAfter,
-            everythingAfterSameLine,
-          };
-        };
-
         const importBlockBeginning =
-          unsortedImportStatements[0].actual.range[0] -
-          details(unsortedImportStatements[0].actual).everythingBefore.length;
+          _.first(unsortedImportStatements)!.actual.range[0] -
+          details(sourceCode, unsortedImportStatements[0].actual).everythingBeforeExceptFirstLine.length;
+
         const importBlockEnd =
-          unsortedImportStatements[unsortedImportStatements.length - 1].actual.range[1] +
-          details(unsortedImportStatements[unsortedImportStatements.length - 1].actual).everythingAfterSameLine.length;
+          _.last(unsortedImportStatements)!.actual.range[1] +
+          details(sourceCode, _.last(unsortedImportStatements)!.actual).everythingAfterSameLine.length;
+
+        const sortedImportStatementsText = unsortedImportStatements
+          .map(({ expected }) => {
+            const { everythingBeforeExceptFirstLine, nodeText, everythingAfterSameLine } = details(
+              sourceCode,
+              expected,
+            );
+
+            return `${everythingBeforeExceptFirstLine}${nodeText}${everythingAfterSameLine}`;
+          })
+          .join('');
 
         context.report({
           node: unsortedImportStatements[0].actual,
           messageId: 'importsMustBeAlphabetized',
-          fix(fixer) {
-            return fixer.replaceTextRange(
-              [importBlockBeginning, importBlockEnd],
-              unsortedImportStatements
-                .map(({ expected }) => {
-                  const { everythingBefore, nodeText, everythingAfterSameLine } = details(expected);
-
-                  return `${everythingBefore}${nodeText}${everythingAfterSameLine}`;
-                })
-                .join('\n'),
-            );
-          },
+          fix: (fixer: RuleFixer): RuleFix =>
+            fixer.replaceTextRange([importBlockBeginning, importBlockEnd], sortedImportStatementsText),
         });
       },
     };
--- test/rules/ordered-imports.test.ts
+++ test/rules/ordered-imports.test.ts
@@ -4,6 +4,12 @@ import { ruleTester } from '../RuleTester';
 ruleTester.run('ordered-imports', rule, {
   valid: [
     {
+      code: ``,
+    },
+    {
+      code: `const a: string = '';`,
+    },
+    {
       code: `
       import 'a';
       import 'b';
@@ -15,25 +21,112 @@ ruleTester.run('ordered-imports', rule, {
       import './b';
       `,
     },
+    {
+      code: `
+      import 'a';
+      import 'b';
+      import '../../a';
+      import '../../b';
+      import '../a';
+      import '../b';
+      import './a';
+      import './b';
+      `,
+    },
+    {
+      code: `
+      import 'a';
+      import 'b';
+      const a: string = ''
+      `,
+    },
   ],
 
   invalid: [
     {
-      code: `import 'b';
-/* comment for a */
-import 'a'; // comment a
-/* comment for c */
-import 'c';
-import 'd'; // comment d
+      code: `
+      import 'b';
+      /* comment for a */
+      import 'a'; // comment a
+      /* comment for c */
+      import 'c';
+      import 'd'; // comment d
       `,
-      errors: [{ messageId: 'importsMustBeAlphabetized', line: 1 }],
+      errors: [{ messageId: 'importsMustBeAlphabetized', line: 2 }],
       output: `
-/* comment for a */
-import 'a'; // comment a
-import 'b';
-/* comment for c */
-import 'c';
-import 'd'; // comment d
+      /* comment for a */
+      import 'a'; // comment a
+      import 'b';
+      /* comment for c */
+      import 'c';
+      import 'd'; // comment d
+      `,
+    },
+    {
+      code: `
+      import 'a';
+      import './a';
+      import '../../a';
+      import '../a';
+      import '../../b';
+      import './b';
+      import 'b';
+      import '../b';
+      `,
+      errors: [{ messageId: 'importsMustBeAlphabetized', line: 2 }],
+      output: `
+      import 'a';
+      import 'b';
+      import '../../a';
+      import '../../b';
+      import '../a';
+      import '../b';
+      import './a';
+      import './b';
+      `,
+    },
+    {
+      code: `
+      import 'a';
+      import 'b';
+
+      import '../../a';
+      import '../../b';
+
+      import '../a';
+      import '../b';
+
+      import './b';
+      import './a';
+      `,
+      errors: [{ messageId: 'importsMustBeAlphabetized', line: 2 }],
+      output: `
+      import 'a';
+      import 'b';
+
+      import '../../a';
+      import '../../b';
+
+      import '../a';
+      import '../b';
+      import './a';
+
+      import './b';
+      `,
+    },
+    {
+      code: `
+      import 'b';
+      import 'a';
+
+      const a: string = ''
+      `,
+      errors: [{ messageId: 'importsMustBeAlphabetized', line: 2 }],
+      output: `
+      import 'a';
+      import 'b';
+
+      const a: string = ''
       `,
     },
   ],
