ca2008880ed631dbf387e6ddd11e8fd1d07da2c8
Dariusz Czajkowski <darek@darek.dev>
2022-06-18 16:32:04 +0200

    Fixed missing rules

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import enumValueName from './rules/enum-value-name';
-// import noRelativeImports from './rules/no-relative-imports';
-// import orderedImports from './rules/ordered-imports';
+import noRelativeImports from './rules/no-relative-imports';
+import orderedImports from './rules/ordered-imports';
 
 export = {
   rules: {
     'enum-value-name': enumValueName,
-    // 'no-relative-imports': noRelativeImports,
-    // 'ordered-imports': orderedImports,
+    'no-relative-imports': noRelativeImports,
+    'ordered-imports': orderedImports,
   },
 };
