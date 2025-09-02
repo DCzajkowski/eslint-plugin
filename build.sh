39553e29f6d09b616830abdfaf793cf444448dd6
Dariusz Czajkowski <darek@darek.dev>
2025-09-02 18:45:12 +0200

    Remove Rollup

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-rollup -c
-mv dist/types/src/index.d.ts dist/index.d.ts
-rm -rf dist/types
-cp dist/index.d.ts dist/index.d.cts
-mv dist/index.d.ts dist/index.d.mts
-sed -i 's/export default/export =/g' dist/index.d.cts
